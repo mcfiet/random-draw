3083983a3aaa2b6941cab73399a86b936b32ceef
Fiete Scheel <fietescheel@gmail.com>
2024-11-24 22:51:56 +0100

    Implement Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface UserData {
+  username: string;
+  password: string;
 }
 
-export default App
+function App() {
+  const url = "http://localhost:3000";
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token"),
+  );
+  const [draw, setDraw] = useState<string | null>(null);
+
+  const getDrawQuery = async () => {
+    try {
+      const response = await fetch(url + "/draw", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token!,
+        },
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Failed to get draw");
+      }
+      const resDraw = await response.text();
+      console.log("Draw: ", resDraw);
+      setDraw(resDraw);
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+      setError(null);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+      getDrawQuery();
+    }
+  }, [token]);
+
+  const loginQuery = async (loginData: UserData) => {
+    try {
+      const response = await fetch(url + "/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to login");
+      }
+      const jwtToken = response.headers.get("Authorization")?.split(" ")[1];
+      setToken(jwtToken ?? "");
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+      setError(null);
+    }
+  };
+
+  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+    const loginData = { username, password };
+    loginQuery(loginData);
+  }
+
+  async function drawHandler() {
+    console.log(url + "/draw");
+    try {
+      const response = await fetch(url + "/draw", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token!,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+      const reciever = await response.text();
+      console.log("drawn user: ", reciever);
+      setDraw(reciever);
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  // if (error) {
+  //   return <h1>{error}</h1>;
+  // }
+
+  if (!token) {
+    return (
+      <>
+        <h1>Anmeldung</h1>
+        <form onSubmit={handleLogin}>
+          <input type="text" name="username" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+        <div>{token}</div>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Hallo {jwtDecode(token).username}</h1>
+      {draw && (
+        <div style={{ margin: "1rem" }}>{`Du hast ${draw} gezogen`}</div>
+      )}
+      <button style={{ marginBottom: "1rem" }} onClick={drawHandler}>
+        Auslosen
+      </button>
+      {error && (
+        <>
+          <div style={{ color: "lightpink" }}>{error}</div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
